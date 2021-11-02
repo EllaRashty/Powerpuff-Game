eac3ed7267d20232cc26f007dd4424348f476c0e
Ella Rashty <48799296+EllaRashty@users.noreply.github.com>
2021-11-02 21:18:30 +0200

    Checks Crash -> vibrate and hearts

--- app/src/main/java/com/example/hw1_ellarashty/MainActivity.java
+++ app/src/main/java/com/example/hw1_ellarashty/MainActivity.java
@@ -5,10 +5,14 @@ package com.example.hw1_ellarashty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
@@ -20,7 +24,6 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView player;
     private ImageView[] hearts, enemy;
     private Button right, left;
 
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private ValueAnimator[] animations;
-    private int animationIndex, screenHeight;
+    private int animationIndex, screenHeight,hearts_count = 3;
 
 
 
@@ -48,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.heart_1), findViewById(R.id.heart_2), findViewById(R.id.heart_3)
         };
 
-        player = findViewById(R.id.player_1);
 
         right = findViewById(R.id.right_BTN);
         right.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
 
         start();
 
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 
     private void start() {
@@ -101,17 +107,54 @@ public class MainActivity extends AppCompatActivity {
 //                        enemy[position].setTranslationY(enemy[position].getTranslationY());
 //                    else
                     enemy[position].setTranslationY(animatedValue);
-//                    if(isCollision(enemy[position],player)) {
-//                        enemy[position].setY(-130);
-//                        checkHit();
-//                        updatedAnimation.start();
-//                    }
+                    if(isCollision(enemy[position],path[playerIndex])) {
+                        enemy[position].setY(-130);
+                        checkCrash();
+                        updatedAnimation.start();
+                    }
 //                    else updateScore(enemy[position],updatedAnimation,enemyMoney);
                 }
             });
         }
     }
+    private void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+    }
 
+    private void checkCrash() {
+        vibrate();
+        if(hearts_count == 1) {
+            hearts[hearts_count - 1].setVisibility(View.INVISIBLE);
+//            pauseGame();
+            finish();
+        }
+        else {
+            hearts[hearts_count - 1].setVisibility(View.INVISIBLE);
+            hearts_count--;
+        }
+    }
+
+    private boolean isCollision(ImageView objectCol, ImageView playerCol) {
+        int[] object_locate = new int[2];
+        int[] player_locate = new int[2];
+
+        objectCol.getLocationOnScreen(object_locate);
+        playerCol.getLocationOnScreen(player_locate);
+        Log.d("pttt", "B player_locate=" + player_locate );
+
+
+        Rect rect1=new Rect(object_locate[0],object_locate[1],(int)(object_locate[0]+ objectCol.getWidth()),(int)(object_locate[1]+objectCol.getHeight()));
+        Rect rect2=new Rect(player_locate[0],player_locate[1],(int)(player_locate[0]+ playerCol.getWidth()),(int)(player_locate[1]+playerCol.getHeight()));
+
+        return Rect.intersects(rect1,rect2);
+    }
 
     private void SetEnemyAnimParameters() {
         final int initialHeight = -(500 + (int) (Math.random() * 1000));
