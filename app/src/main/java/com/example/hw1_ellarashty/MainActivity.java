5066ef49bfd7fe9da20cf97546fcea27310f635a
Ella Rashty <48799296+EllaRashty@users.noreply.github.com>
2021-11-09 20:23:12 +0200

    Add comments

--- app/src/main/java/com/example/hw1_ellarashty/MainActivity.java
+++ app/src/main/java/com/example/hw1_ellarashty/MainActivity.java
@@ -10,10 +10,8 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -21,23 +19,24 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Buttons for moving the player
     private Button right, left;
 
     private ImageView [] path, hearts;
     private int playerIndex, hearts_count = 3;;
 
+    //Enemy movement
     private ImageView[] enemy;
     private ValueAnimator[] enemy_animations;
     private int animationIndex, screenHeight;
 
+    //Duration of the play
     private TextView duration_time;
     private Timer timer ;
     int clock = 0;
@@ -52,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        duration_time = findViewById(R.id.duration_time);
 
         findViews();
         initView();
@@ -72,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        //Movement right
         right = findViewById(R.id.right_BTN);
         right.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Movement left
         left = findViewById(R.id.left_BTN);
         left.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,8 +111,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onAnimationUpdate(ValueAnimator updatedAnimation) {
                     int animatedValue =(int) updatedAnimation.getAnimatedValue();
+                    //Movement of the enemy
                     enemy[enemyIndex].setTranslationY(animatedValue);
-                    if(isCollision(enemy[enemyIndex],path[playerIndex])) {
+                    //Checks if there is a collision
+                    if(checkCrash(enemy[enemyIndex],path[playerIndex])) {
                         enemy[enemyIndex].setY(-120);
                         updateCrash();
                         updatedAnimation.start();
@@ -145,39 +147,44 @@ public class MainActivity extends AppCompatActivity {
         vibrate();
         hearts[hearts_count - 1].setVisibility(View.INVISIBLE);
         hearts_count--;
+        //To know which Power Puff girl is playing
         if (hearts_count == 0)
             finish();
         else if (hearts_count == 2) {
             path[playerIndex].setImageResource(R.drawable.img_blossom);
+            //Animation for a player replacement
             anima = AnimationUtils.loadAnimation(this, R.anim.rotate);
             path[playerIndex].startAnimation(anima);
         }
         else {
             path[playerIndex].setImageResource(R.drawable.img_bubbles);
+            //Animation for a player replacement
             anima = AnimationUtils.loadAnimation(this, R.anim.rotate);
             path[playerIndex].startAnimation(anima);
         }
     }
 
-    private boolean isCollision(ImageView objectCol, ImageView playerCol) {
+    //Checks if two objects in the same position (collision)
+    private boolean checkCrash(ImageView enemy_col, ImageView player_col) {
         int[] object_locate = new int[path.length];
         int[] player_locate = new int[path.length];
 
-        objectCol.getLocationOnScreen(object_locate);
-        playerCol.getLocationOnScreen(player_locate);
+        enemy_col.getLocationOnScreen(object_locate);
+        player_col.getLocationOnScreen(player_locate);
 
-        Rect rect1=new Rect(object_locate[0],object_locate[1],(int)(object_locate[0]+ objectCol.getWidth()),(int)(object_locate[1]+objectCol.getHeight()));
-        Rect rect2=new Rect(player_locate[0],player_locate[1],(int)(player_locate[0]+ playerCol.getWidth()),(int)(player_locate[1]+playerCol.getHeight()));
+        Rect rect1=new Rect(object_locate[0],object_locate[1],(int)(object_locate[0]+ enemy_col.getWidth()),(int)(object_locate[1]+enemy_col.getHeight()));
+        Rect rect2=new Rect(player_locate[0],player_locate[1],(int)(player_locate[0]+ player_col.getWidth()),(int)(player_locate[1]+player_col.getHeight()));
 
         return Rect.intersects(rect1,rect2);
     }
 
-
-
     private void findViews() {
+        duration_time = findViewById(R.id.duration_time);
+        //The path of the player
         path = new ImageView[]{
                 findViewById(R.id.player_0), findViewById(R.id.player_1), findViewById(R.id.player_2)
         };
+
         enemy = new ImageView[]{
                 findViewById(R.id.enemy_1), findViewById(R.id.enemy_2), findViewById(R.id.enemy_3)
         };
@@ -185,11 +192,16 @@ public class MainActivity extends AppCompatActivity {
         hearts = new ImageView[]{
                 findViewById(R.id.heart_1), findViewById(R.id.heart_2), findViewById(R.id.heart_3)
         };
+
+        //Initial position of the player
         playerIndex = 1;
     }
 
+
+    //Move the player to the left
     public void moveRight(View view) {
         path[playerIndex].setImageResource(0);
+        //To know which Power Puff girl is playing
         if(hearts_count==3)
             path[playerIndex+1].setImageResource(R.drawable.img_buttercup);
         else if(hearts_count==2)
@@ -200,8 +212,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Move the player to the right
     public void moveLeft(View view) {
         path[playerIndex].setImageResource(0);
+        //To know which Power Puff girl is playing
         if(hearts_count==3)
             path[playerIndex - 1].setImageResource(R.drawable.img_buttercup);
         else if(hearts_count==2)
@@ -211,6 +225,7 @@ public class MainActivity extends AppCompatActivity {
         playerIndex--;
     }
 
+    // Duration of the play
     private void startTicker() {
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -234,6 +249,8 @@ public class MainActivity extends AppCompatActivity {
     private void updateClock() {
         clock++;
         duration_time.setText("Time: " + clock);
+
+        //After every 20 seconds an animation will be activated
         if(clock%20==0){
             anima = AnimationUtils.loadAnimation(this, R.anim.sample_anim);
             duration_time.startAnimation(anima);
